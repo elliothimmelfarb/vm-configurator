d57c1c0406d28cb542b2ddc4d963ffd41a6e06a7
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-12-10 14:16:58 -0700

    material UI added and base CSS and main menu buttons created

--- app/index.js
+++ app/index.js
@@ -4,6 +4,11 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+
 import routes from './routes';
 import configureStore from './store/configureStore';
 import './app.global.css';
@@ -11,9 +16,13 @@ import './app.global.css';
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
 
+injectTapEventPlugin();
+
 render(
   <Provider store={store}>
-    <Router history={history} routes={routes} />
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <Router history={history} routes={routes} />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
